8a9c9afa200edecfa519fb5b4f2c1e50e40be19f
maurirandom-del <maurirandom@gmail.com>
2026-06-20 23:20:02 -0600

    Tienda completa Tokki V2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,18 +10,33 @@
 
       <!-- Empiezan los productos -->
     @foreach ($productos as $producto)
-    <div class="col-6 col-sm-6 col-md-4 col-lg-3">
-      <div class="card">
-        <div class="ratio ratio-1x1">
-          <img src="{{ $producto->imagen_url }}" class="card-img-top rounded-0" alt="{{ $producto->nombre }}"> 
+    @if($producto->stock >= 1)
+      <div class="col-6 col-sm-6 col-md-4 col-lg-3">
+        <div class="card">
+          <div class="ratio ratio-1x1">
+            <img src="{{ $producto->imagen_url }}" class="card-img-top rounded-0" alt="{{ $producto->nombre }}"> 
+          </div>
+          <form action="{{ route('favoritos.agregar', $producto->id_producto) }}" method="POST">
+            @csrf
+            <button class="btn btn-favorite fs-5">
+              <i class="fa-solid fa-star"></i>
+            </button>
+          </form>
+
+          <div class="card-body text-center">
+            <h5 class="card-title">{{ $producto->nombre }}</h5>
+            <p class="card-text text-muted">${{ number_format($producto->precio, 2) }}</p>
+
+            <form action="{{ route('carrito.agregar', $producto->id_producto) }}" method="POST">
+              @csrf
+              <button class="btn btn-custom w-100 fs-4">
+                Añadir al carrito
+                <i class="ps-2 pt-1 fa-solid fa-cart-plus"></i>
+              </button>
+            </form>
+          </div>
         </div>
-        <button class="btn btn-favorite fs-5"><i class="fa-solid fa-star"></i></button>
-        <div class="card-body text-center">
-          <h5 class="card-title">{{ $producto->nombre }}</h5>
-          <p class="card-text text-muted">${{ $producto->precio }}</p>
-          <button class="btn btn-custom w-100 fs-4">Añadir al carrito<i class="ps-2 pt-1 fa-solid fa-cart-plus"></i></button>
-        </div>
-      </div>
-    </div>   
+      </div> 
+      @endif  
     @endforeach
     @endsection
